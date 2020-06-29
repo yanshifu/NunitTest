6cddd9799af07a2e25483382f1c988d928e061f6
yanshifu <yanshifu2012@hotmail.com>
2020-06-29 09:32:17 +0900

    aaa

---
+++ UNitTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UNitTest
+{
+    class Program
+    {
+        public bool IsValid(int opt)
+        {
+            if (opt > 100)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int AddData(int a,int b)
+        {
+            return (a + b);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
